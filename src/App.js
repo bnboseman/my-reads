a1b7a3ba891d063075f00d7f3719924fca265d96
Nichole Boseman <bnboseman@gmail.com>
2017-07-27 07:31:14 -0400

    Added Book and BookShelf

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom'
 import * as BooksAPI from './BooksAPI'
+import BookShelf from './BookShelf'
 import './App.css';
 
 class BooksApp extends Component {
+    state = {
+        books: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books: books})
+        })
+    }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <Route path="/" exact render={() => (
+              <div className="list-books">
+                  <div className="list-books-title">
+                      <h1>My Reads</h1>
+                  </div>
+                  <div className="list-books-content">
+                      <div>
+                          <BookShelf title="Currently Reading" books={this.state.books.filter( book => {return book.shelf === 'currentlyReading'})}/>
+
+                          <BookShelf title="Want to Read" books={this.state.books.filter( book => {return book.shelf === 'wantToRead'})}/>
+
+                          <BookShelf title="Read" books={this.state.books.filter( book => {return book.shelf === 'read'})}/>
+                      </div>
+                  </div>
+              </div>
+          )} />
+
       </div>
     );
   }
